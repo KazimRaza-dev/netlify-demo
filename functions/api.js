8a464b65a6f026812272c873cd3402f8f0880512
kazim raza <kazim.raza@tintash.com>
2023-01-13 12:53:31 +0500

    what: ready to deploy

--- functions/api.js
+++ functions/api.js
@@ -1,31 +1,29 @@
-const express =  require('express');
+const express = require('express');
 const serverless = require('serverless-http');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 const router = express.Router();
-app.use(cors())
+// app.use(cors());
 
 router.get('/', cors(), (req, res) => {
-    res.json(
-        [
-            {
-                'id': '001',
-                'name': 'Smith',
-                'email': 'smith@gmail.com'
-            },
-            {
-                'id': '002',
-                'name': 'Sam',
-                'email': 'sam@gmail.com'
-            },
-            {
-                'id': '003',
-                'name': 'lily',
-                'email': 'lily@gmail.com'
-            }
-        ]
-    )
-})
+  res.json([
+    {
+      id: '001',
+      name: 'Smith',
+      email: 'smith@gmail.com',
+    },
+    {
+      id: '002',
+      name: 'Sam',
+      email: 'sam@gmail.com',
+    },
+    {
+      id: '003',
+      name: 'lily',
+      email: 'lily@gmail.com',
+    },
+  ]);
+});
 
 app.use('/', router);
 module.exports.handler = serverless(app);
